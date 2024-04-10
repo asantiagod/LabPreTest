fa139b74a4785cbaf053246760c0ccc839074882
asantiagode <asantiago.duque@udea.edu.co>
2024-04-09 23:39:53 -0500

    chore: setup database

---
+++ LabPreTest.Backend/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using LabPreTest.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabPreTest.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        // for each database entity you need create a DbSet
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
---
+++ LabPreTest.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabPreTest.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Country")]
+        [MaxLength(100, ErrorMessage = "The field {0} can't be more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is required.")]
+        public string Name { get; set; } = null!;
+    }
+}
