c98e92d3d61cbaf7e1d6a9bd3731d44efee3b9ee
asantiagode <asantiago.duque@udea.edu.co>
2024-04-23 21:59:29 -0500

    wip: patient repository (error status 500 appears)

--- LabPreTest.Backend/Controllers/PatientController.cs
+++ LabPreTest.Backend/Controllers/PatientController.cs
@@ -1,66 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using LabPreTest.Backend.Data;
 using LabPreTest.Shared.Entities;
+using LabPreTest.Backend.UnitOfWork.Interfaces;
 
 namespace LabPreTest.Backend.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
 
-    public class PatientsController : ControllerBase
+    public class PatientsController : GenericController<Patient>
     {
-        private readonly DataContext _context;
 
-        public PatientsController(DataContext context)
+        public PatientsController(IGenericUnitOfWork<Patient> unitOfWork) : base(unitOfWork)
         {
-            _context = context;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetAsync()
-        {
-            return Ok(await _context.Patients.AsNoTracking().ToListAsync());
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetAsync(int id)
-        {
-            var patient = await _context.Patients.FindAsync(id);
-            if (patient == null)
-            {
-                return NotFound();
-            }
-            return Ok(patient);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> PostAsync(Patient patient)
-        {
-            _context.Add(patient);
-            await _context.SaveChangesAsync();
-            return Ok(patient);
-        }
-
-        [HttpPut]
-        public async Task<IActionResult> PutAsync(Patient patient)
-        {
-            _context.Update(patient);
-            await _context.SaveChangesAsync();
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAsync(int id)
-        {
-            var patient = await _context.Patients.FindAsync(id);
-            if (patient == null)
-            {
-                return NotFound();
-            }
-            _context.Remove(patient);
-            await _context.SaveChangesAsync();
-            return NoContent();
         }
     }
 }
