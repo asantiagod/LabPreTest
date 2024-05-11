555893c45c26e509a6a8b6ba12c07e2d3040c028
asantiagode <asantiago.duque@udea.edu.co>
2024-05-11 17:36:35 -0500

    feat: add user photo on registration

---
+++ LabPreTest.Frontend/Shared/AuthLinks.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop.Implementation;
+
+namespace LabPreTest.Frontend.Shared
+{
+    public partial class AuthLinks
+    {
+        public string? photoUser;
+
+        [CascadingParameter]
+        private Task<AuthenticationState> AuthenticationStateTask { get; set; } = null!;
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var authenticationState = await AuthenticationStateTask;
+            var claims = authenticationState.User.Claims.ToList();
+            var photoClaim = claims.FirstOrDefault(x => x.Type == "Photo");
+            
+            if(photoClaim != null)
+                photoUser = photoClaim.Value;
+        }
+    }
+}
