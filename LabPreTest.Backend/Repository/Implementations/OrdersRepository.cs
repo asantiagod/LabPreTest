ff68f5aa3691b1691567b2dfab1f084b5d2e5aac
asantiagode <santiago.duque@fracttal.com>
2024-06-07 16:27:07 -0500

    wip: order movement

---
+++ LabPreTest.Backend/Repository/Implementations/OrdersRepository.cs
@@ -0,0 +1,49 @@
+﻿using LabPreTest.Backend.Data;
+using LabPreTest.Backend.Repository.Interfaces;
+using LabPreTest.Shared.DTO;
+using LabPreTest.Shared.Entities;
+using LabPreTest.Shared.Responses;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabPreTest.Backend.Repository.Implementations
+{
+    public class OrdersRepository : GenericRepository<Order>, IOrdersRepository
+    {
+        private readonly DataContext _context;
+        private readonly IUsersRepository _usersRepository;
+
+        public OrdersRepository(DataContext context, IUsersRepository usersRepository) : base(context)
+        {
+            _context = context;
+            _usersRepository = usersRepository;
+        }
+
+        public async Task<ActionResponse<IEnumerable<Order>>> GetAsync(string email, PagingDTO pagination)
+        {
+            var user = await _usersRepository.GetUserAsync(email);
+            if (user == null)
+            {
+                return new ActionResponse<IEnumerable<Order>>
+                {
+                    WasSuccess = false,
+                    Message = "Usuario no valido"
+                };
+            }
+
+            //var query = _context.Orders
+            //    .Include(o => o.User)
+
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResponse<int>> GetTotalPagesAsync(string email, PagingDTO pagination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResponse<Order>> UpdateFullAsync(string email, OrderDTO orderDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LabPreTest.Shared/DTO/OrderDTO.cs
@@ -0,0 +1,15 @@
+﻿using LabPreTest.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabPreTest.Shared.DTO
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
