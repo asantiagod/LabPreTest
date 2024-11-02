cd432020b7f2b469e4c2ba7ae1967818818f9f5c
Carlos Alzate <carlosm8561@gmail.com>
2024-11-02 13:34:02 -0500

    add migrations to fix birthday date column

---
+++ LabPreTest.Backend/Migrations/20241102182937_begin_to_do_frontend_changes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LabPreTest.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class begin_to_do_frontend_changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDay",
+                table: "Patients",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDay",
+                table: "Medicians",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BirthDay",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BirthDay",
+                table: "Medicians",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+    }
+}
