e3d72a886f308ce37d2b3732551611a2e3e84797
asantiagode <asantiago.duque@udea.edu.co>
2024-11-14 21:42:28 -0500

    chore: add gender field to the patient and medic entities

---
+++ LabPreTest.Backend/Migrations/20241115023111_add_missing_fields.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LabPreTest.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_missing_fields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "Patients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "Medicians",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Medicians");
+        }
+    }
+}
