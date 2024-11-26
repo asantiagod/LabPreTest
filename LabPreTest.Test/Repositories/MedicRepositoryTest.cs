6a3be8a98a6688e17ebf57880da193971ad074cf
asantiagode <asantiago.duque@udea.edu.co>
2024-11-25 22:01:06 -0500

    feat: add unit tests related to patient and medic entities

--- LabPreTest.Test/Repositories/MedicRepositoryTest.cs
+++ LabPreTest.Test/Repositories/MedicRepositoryTest.cs
@@ -12,11 +12,11 @@ using System.Security.Claims;
 namespace LabPreTest.Test.Repositories
 {
     [TestClass]
-    public class PatientRepositoryTest
+    public class MedicRepositoryTest
     {
         private Mock<IHttpContextAccessor> _mockContextAccessor = null!;
         private DataContext _dataContext = null!;
-        private PatientRepository _patientRepository = null!;
+        private MedicianRepository _medicianRepository = null!;
 
         [TestInitialize]
         public void Setup()
@@ -32,16 +32,16 @@ namespace LabPreTest.Test.Repositories
             _mockContextAccessor.Setup(_ => _.HttpContext).Returns(httpContext);
 
             _dataContext = new DataContext(options, _mockContextAccessor.Object);
-            _patientRepository = new PatientRepository(_dataContext);
+            _medicianRepository = new MedicianRepository(_dataContext);
 
-            _dataContext.Patients.AddRange(new List<Patient>
+            _dataContext.Medicians.AddRange(new List<Medic>
             {
-                new Patient{ Id = 1, DocumentId="123456", Name="user ID_1", Gender=GenderType.Female},
-                new Patient{ Id = 2, DocumentId="234561", Name="user ID_2", Gender=GenderType.Male},
-                new Patient{ Id = 3, DocumentId="345612", Name="user ID_3", Gender=GenderType.Female},
-                new Patient{ Id = 4, DocumentId="456123", Name="user ID_4", Gender=GenderType.Male},
-                new Patient{ Id = 5, DocumentId="561234", Name="user ID_5", Gender=GenderType.Female},
-                new Patient{ Id = 6, DocumentId="612345", Name="user ID_6", Gender=GenderType.Male},
+                new Medic { Id = 1, DocumentId="123456", Name="user ID_1", Gender=GenderType.Female},
+                new Medic { Id = 2, DocumentId="234561", Name="user ID_2", Gender=GenderType.Male},
+                new Medic { Id = 3, DocumentId="345612", Name="user ID_3", Gender=GenderType.Female},
+                new Medic { Id = 4, DocumentId="456123", Name="user ID_4", Gender=GenderType.Male},
+                new Medic { Id = 5, DocumentId="561234", Name="user ID_5", Gender=GenderType.Female},
+                new Medic { Id = 6, DocumentId="612345", Name="user ID_6", Gender=GenderType.Male},
             });
             _dataContext.SaveChanges();
         }
@@ -57,30 +57,30 @@ namespace LabPreTest.Test.Repositories
         public async Task GetAsync_ReturnsSuccessfulWhenPatientIsFound()
         {
             // Arrange
-            int patientId = 1;
+            int medicId = 1;
 
             // Act
-            var response = await _patientRepository.GetAsync(patientId);
+            var response = await _medicianRepository.GetAsync(medicId);
 
             // Assert
             Assert.IsTrue(response.WasSuccess);
-            var patient = response.Result!;
-            Assert.AreEqual(patient.Name, "user ID_1");
+            var medic = response.Result!;
+            Assert.AreEqual(medic.Name, "user ID_1");
         }
 
         [TestMethod]
         public async Task GetAsync_ReturnsErrorMessageWhenPatientIsNotFound()
         {
             // Arrange
-            int patientId = 123;
+            int medicId = 123;
 
             // Act
-            var response = await _patientRepository.GetAsync(patientId);
+            var response = await _medicianRepository.GetAsync(medicId);
 
             // Assert
             Assert.IsFalse(response.WasSuccess);
             var errorMessage = response.Message;
-            Assert.AreEqual(errorMessage, MessageStrings.DbRecordNotFoundMessage);
+            Assert.AreEqual(errorMessage, MessageStrings.DbMedicianNotFoundMessage);
         }
 
         [TestMethod]
@@ -89,7 +89,7 @@ namespace LabPreTest.Test.Repositories
             //Arrange
 
             // Act
-            var response = await _patientRepository.GetAsync();
+            var response = await _medicianRepository.GetAsync();
 
             // Assert
             Assert.IsTrue(response.WasSuccess);
@@ -101,18 +101,18 @@ namespace LabPreTest.Test.Repositories
         public async Task GetAsync_ReturnsAFilteredMedic()
         {
             // Arrange
-            var pagingDTO = new PagingDTO { Filter = "testpatient" };
-            var patient = new Patient { DocumentId = "1122334455", Name = "TestPatient", Gender = GenderType.Female };
-            _dataContext.Patients.Add(patient);
+            var pagingDTO = new PagingDTO { Filter = "testmedic" };
+            var medic = new Medic { DocumentId = "1122334455", Name = "TestMedician", Gender = GenderType.Female };
+            _dataContext.Medicians.Add(medic);
             _dataContext.SaveChanges();
 
             // Act
-            var response = await _patientRepository.GetAsync(pagingDTO);
+            var response = await _medicianRepository.GetAsync(pagingDTO);
 
             Assert.IsTrue(response.WasSuccess);
-            var filteredPatients = response.Result!;
-            Assert.AreEqual(1, filteredPatients.Count());
-            Assert.AreEqual(patient, filteredPatients.First());
+            var filteredMedics = response.Result!;
+            Assert.AreEqual(1, filteredMedics.Count());
+            Assert.AreEqual(medic, filteredMedics.First());
         }
 
         [TestMethod]
@@ -122,11 +122,11 @@ namespace LabPreTest.Test.Repositories
             var pagingDTO = new PagingDTO();
 
             // Act
-            var response = await _patientRepository.GetAsync(pagingDTO);
+            var response = await _medicianRepository.GetAsync(pagingDTO);
 
             Assert.IsTrue(response.WasSuccess);
-            var filteredPatients = response.Result!;
-            Assert.AreEqual(6, filteredPatients.Count());
+            var filteredMedics = response.Result!;
+            Assert.AreEqual(6, filteredMedics.Count());
         }
 
         [TestMethod]
@@ -136,7 +136,7 @@ namespace LabPreTest.Test.Repositories
             var pagingDTO = new PagingDTO();
 
             // Act
-            var response = await _patientRepository.GetTotalPagesAsync(pagingDTO);
+            var response = await _medicianRepository.GetTotalPagesAsync(pagingDTO);
 
             Assert.IsTrue(response.WasSuccess);
             Assert.AreEqual(1, response.Result);
@@ -149,7 +149,7 @@ namespace LabPreTest.Test.Repositories
             var pagingDTO = new PagingDTO { Filter = "user",RecordsNumber = 1 };
 
             // Act
-            var response = await _patientRepository.GetTotalPagesAsync(pagingDTO);
+            var response = await _medicianRepository.GetTotalPagesAsync(pagingDTO);
 
             Assert.IsTrue(response.WasSuccess);
             Assert.AreEqual(6, response.Result);
@@ -162,7 +162,7 @@ namespace LabPreTest.Test.Repositories
             string documentId = "123456";
 
             // Act
-            var response = await _patientRepository.GetAsync(documentId);
+            var response = await _medicianRepository.GetAsync(documentId);
 
             Assert.IsTrue(response.WasSuccess);
             var resultMedic = response.Result!;
@@ -176,10 +176,9 @@ namespace LabPreTest.Test.Repositories
             string documentId = "111111";
 
             // Act
-            var response = await _patientRepository.GetAsync(documentId);
+            var response = await _medicianRepository.GetAsync(documentId);
 
             Assert.IsFalse(response.WasSuccess);
         }
-
     }
 }
---
+++ LabPreTest.Test/UnitsOfWork/MedicUnitOfWorkTest.cs
@@ -0,0 +1,100 @@
+﻿using LabPreTest.Backend.Repository.Interfaces;
+using LabPreTest.Backend.UnitOfWork.Implementations;
+using LabPreTest.Shared.DTO;
+using LabPreTest.Shared.Entities;
+using LabPreTest.Shared.Responses;
+using Moq;
+
+namespace LabPreTest.Test.UnitsOfWork
+{
+    [TestClass]
+    public class MedicUnitOfWorkTest
+    {
+        private Mock<IGenericRepository<Medic>> _mockGenericRepository = null!;
+        private Mock<IMedicianRepository> _mockMediciansRepository = null!;
+        private MediciansUnitOfWork _mediciansUnitOfWork = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockGenericRepository = new Mock<IGenericRepository<Medic>>();
+            _mockMediciansRepository = new Mock<IMedicianRepository>();
+            _mediciansUnitOfWork = new MediciansUnitOfWork(_mockGenericRepository.Object,
+                                                       _mockMediciansRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAsync_CallsRepositoryAndReturnsResult()
+        {
+            var expectedMedicians = new List<Medic> { new Medic() };
+            var response = new ActionResponse<IEnumerable<Medic>> { WasSuccess = true, Result = expectedMedicians };
+            _mockMediciansRepository.Setup(x => x.GetAsync()).ReturnsAsync(response);
+
+            var result = await _mediciansUnitOfWork.GetAsync();
+
+            Assert.AreEqual(response, result);
+            _mockMediciansRepository.Verify(x => x.GetAsync(), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_CallsRepositoryByIdAndReturnsResult()
+        {
+            var patient = new Medic();
+            var response = new ActionResponse<Medic> { WasSuccess = true, Result = patient };
+            int id = 1;
+            _mockMediciansRepository.Setup(x => x.GetAsync(id)).ReturnsAsync(response);
+
+            var result = await _mediciansUnitOfWork.GetAsync(id);
+
+            Assert.AreEqual(response, result);
+            _mockMediciansRepository.Verify(x => x.GetAsync(id), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_CallsRepositoryByPaginAndReturnsResult()
+        {
+            var pagingDTO = new PagingDTO();
+            var response = new ActionResponse<IEnumerable<Medic>>
+            {
+                WasSuccess = true,
+                Result = new List<Medic>()
+            };
+            _mockMediciansRepository.Setup(x => x.GetAsync(pagingDTO)).ReturnsAsync(response);
+
+            var result = await _mediciansUnitOfWork.GetAsync(pagingDTO);
+
+            Assert.AreEqual(response, result);
+            _mockMediciansRepository.Verify(x => x.GetAsync(pagingDTO), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetTotalPagesAsync_CallsRepositoryByPaginAndReturnsResult()
+        {
+            var pagingDTO = new PagingDTO();
+            var response = new ActionResponse<int>
+            {
+                WasSuccess = true,
+                Result = 5
+            };
+            _mockMediciansRepository.Setup(x => x.GetTotalPagesAsync(pagingDTO)).ReturnsAsync(response);
+
+            var result = await _mediciansUnitOfWork.GetTotalPagesAsync(pagingDTO);
+
+            Assert.AreEqual(response, result);
+            _mockMediciansRepository.Verify(x => x.GetTotalPagesAsync(pagingDTO), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_CallsRepositoryByDocumentIdAndReturnsResult()
+        {
+            var medic = new Medic { Name = "Some Medic", DocumentId = "123456" };
+            var response = new ActionResponse<Medic> { WasSuccess = true, Result = medic };
+            _mockMediciansRepository.Setup(x => x.GetAsync(medic.DocumentId)).ReturnsAsync(response);
+
+            var result = await _mediciansUnitOfWork.GetAsync(medic.DocumentId);
+
+            Assert.AreEqual(response, result);
+            _mockMediciansRepository.Verify(x => x.GetAsync(medic.DocumentId), Times.Once());
+        }
+    }
+}
