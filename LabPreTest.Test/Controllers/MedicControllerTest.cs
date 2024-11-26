d8faf996768a2c8125a3d94262aa70e34c905363
asantiagode <asantiago.duque@udea.edu.co>
2024-11-25 22:12:06 -0500

    fix: medic controller tests

--- LabPreTest.Test/Controllers/MedicControllerTest.cs
+++ LabPreTest.Test/Controllers/MedicControllerTest.cs
@@ -11,47 +11,47 @@ namespace LabPreTest.Test.Controllers
     [TestClass]
     public class MedicControllerTest
     {
-        private Mock<IGenericUnitOfWork<Patient>> _mockGenericUnitOfWork = null!;
-        private Mock<IPatientUnitOfWork> _mockPatientUnitOfWork = null!;
-        private PatientsController _patientsController = null!;
+        private Mock<IGenericUnitOfWork<Medic>> _mockGenericUnitOfWork = null!;
+        private Mock<IMedicianUnitOfWork> _mockMedicUnitOfWork = null!;
+        private MedicsController _mediciansController = null!;
 
         [TestInitialize]
         public void Setup()
         {
-            _mockGenericUnitOfWork = new Mock<IGenericUnitOfWork<Patient>>();
-            _mockPatientUnitOfWork = new Mock<IPatientUnitOfWork>();
-            _patientsController = new PatientsController(_mockGenericUnitOfWork.Object,
-                                                         _mockPatientUnitOfWork.Object);
+            _mockGenericUnitOfWork = new Mock<IGenericUnitOfWork<Medic>>();
+            _mockMedicUnitOfWork = new Mock<IMedicianUnitOfWork>();
+            _mediciansController = new MedicsController(_mockGenericUnitOfWork.Object,
+                                                         _mockMedicUnitOfWork.Object);
         }
 
         [TestMethod]
         public async Task GetAsync_ReturnsOK_WhenWasSuccessIsTrue()
         {
             var pagingDTO = new PagingDTO();
-            var response = new ActionResponse<IEnumerable<Patient>> { WasSuccess = true };
-            _mockPatientUnitOfWork.Setup(x => x.GetAsync(pagingDTO)).ReturnsAsync(response);
+            var response = new ActionResponse<IEnumerable<Medic>> { WasSuccess = true };
+            _mockMedicUnitOfWork.Setup(x => x.GetAsync(pagingDTO)).ReturnsAsync(response);
 
-            var result = await _patientsController.GetAsync(pagingDTO);
+            var result = await _mediciansController.GetAsync(pagingDTO);
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
             var okResult = result as OkObjectResult;
             Assert.AreEqual(response.Result, okResult!.Value);
-            _mockPatientUnitOfWork?.Verify(x => x.GetAsync(pagingDTO), Times.Once());
+            _mockMedicUnitOfWork?.Verify(x => x.GetAsync(pagingDTO), Times.Once());
         }
 
         [TestMethod]
         public async Task GetAsync_ReturnsBadrequest_WhenWasSuccessIsFalse()
         {
             var pagingDTO = new PagingDTO();
-            var response = new ActionResponse<IEnumerable<Patient>> { WasSuccess = false };
-            _mockPatientUnitOfWork.Setup(x => x.GetAsync(pagingDTO)).ReturnsAsync(response);
+            var response = new ActionResponse<IEnumerable<Medic>> { WasSuccess = false };
+            _mockMedicUnitOfWork.Setup(x => x.GetAsync(pagingDTO)).ReturnsAsync(response);
 
-            var result = await _patientsController.GetAsync(pagingDTO);
+            var result = await _mediciansController.GetAsync(pagingDTO);
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(BadRequestResult));
-            _mockPatientUnitOfWork?.Verify(x => x.GetAsync(pagingDTO), Times.Once());
+            _mockMedicUnitOfWork?.Verify(x => x.GetAsync(pagingDTO), Times.Once());
         }
 
         [TestMethod]
@@ -59,15 +59,15 @@ namespace LabPreTest.Test.Controllers
         {
             var pagingDTO = new PagingDTO();
             var response = new ActionResponse<int> { WasSuccess = true, Result = 5 };
-            _mockPatientUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagingDTO)).ReturnsAsync(response);
+            _mockMedicUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagingDTO)).ReturnsAsync(response);
 
-            var result = await _patientsController.GetPagesAsync(pagingDTO);
+            var result = await _mediciansController.GetPagesAsync(pagingDTO);
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
             var okResult = (OkObjectResult)result;
             Assert.AreEqual(response.Result, okResult.Value);
-            _mockPatientUnitOfWork?.Verify(x => x.GetTotalPagesAsync(pagingDTO), Times.Once());
+            _mockMedicUnitOfWork?.Verify(x => x.GetTotalPagesAsync(pagingDTO), Times.Once());
         }
 
         [TestMethod]
@@ -75,102 +75,102 @@ namespace LabPreTest.Test.Controllers
         {
             var pagingDTO = new PagingDTO();
             var response = new ActionResponse<int> { WasSuccess = false};
-            _mockPatientUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagingDTO)).ReturnsAsync(response);
+            _mockMedicUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagingDTO)).ReturnsAsync(response);
 
-            var result = await _patientsController.GetPagesAsync(pagingDTO);
+            var result = await _mediciansController.GetPagesAsync(pagingDTO);
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(BadRequestResult));
-            _mockPatientUnitOfWork?.Verify(x => x.GetTotalPagesAsync(pagingDTO), Times.Once());
+            _mockMedicUnitOfWork?.Verify(x => x.GetTotalPagesAsync(pagingDTO), Times.Once());
         }
 
         [TestMethod]
         public async Task GetAsyncFull_ReturnsOK_WhenWasSuccessIsTrue()
         {
-            var response = new ActionResponse<IEnumerable<Patient>> { WasSuccess = true };
-            _mockPatientUnitOfWork.Setup(x => x.GetAsync()).ReturnsAsync(response);
+            var response = new ActionResponse<IEnumerable<Medic>> { WasSuccess = true };
+            _mockMedicUnitOfWork.Setup(x => x.GetAsync()).ReturnsAsync(response);
 
-            var result = await _patientsController.GetAsync();
+            var result = await _mediciansController.GetAsync();
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
             var okResult = result as OkObjectResult;
             Assert.AreEqual(response.Result, okResult!.Value);
-            _mockPatientUnitOfWork?.Verify(x => x.GetAsync(), Times.Once());
+            _mockMedicUnitOfWork?.Verify(x => x.GetAsync(), Times.Once());
         }
 
         [TestMethod]
         public async Task GetAsyncFull_ReturnsBadrequest_WhenWasSuccessIsFalse()
         {
-            var response = new ActionResponse<IEnumerable<Patient>> { WasSuccess = false };
-            _mockPatientUnitOfWork.Setup(x => x.GetAsync()).ReturnsAsync(response);
+            var response = new ActionResponse<IEnumerable<Medic>> { WasSuccess = false };
+            _mockMedicUnitOfWork.Setup(x => x.GetAsync()).ReturnsAsync(response);
 
-            var result = await _patientsController.GetAsync();
+            var result = await _mediciansController.GetAsync();
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(BadRequestResult));
-            _mockPatientUnitOfWork?.Verify(x => x.GetAsync(), Times.Once());
+            _mockMedicUnitOfWork?.Verify(x => x.GetAsync(), Times.Once());
         }
 
         [TestMethod]
         public async Task GetAsyncId_ReturnsOK_WhenWasSuccessIsTrue()
         {
-            var patient = new Patient { Name = "some name", DocumentId = "123456" };
-            var response = new ActionResponse<Patient> { WasSuccess = true, Result = patient };
+            var patient = new Medic { Name = "some name", DocumentId = "123456" };
+            var response = new ActionResponse<Medic> { WasSuccess = true, Result = patient };
             int patientId = 123;
-            _mockPatientUnitOfWork.Setup(x => x.GetAsync(patientId)).ReturnsAsync(response);
+            _mockMedicUnitOfWork.Setup(x => x.GetAsync(patientId)).ReturnsAsync(response);
 
-            var result = await _patientsController.GetAsync(patientId);
+            var result = await _mediciansController.GetAsync(patientId);
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
             var okResult = result as OkObjectResult;
             Assert.AreEqual(response.Result, okResult!.Value);
-            _mockPatientUnitOfWork?.Verify(x => x.GetAsync(patientId), Times.Once());
+            _mockMedicUnitOfWork?.Verify(x => x.GetAsync(patientId), Times.Once());
         }
 
         [TestMethod]
         public async Task GetAsyncId_ReturnsNotFound_WhenWasSuccessIsFalse()
         {
-            var response = new ActionResponse<Patient> { WasSuccess = false };
+            var response = new ActionResponse<Medic> { WasSuccess = false };
             int patientId = 123;
-            _mockPatientUnitOfWork.Setup(x => x.GetAsync(patientId)).ReturnsAsync(response);
+            _mockMedicUnitOfWork.Setup(x => x.GetAsync(patientId)).ReturnsAsync(response);
 
-            var result = await _patientsController.GetAsync(patientId);
+            var result = await _mediciansController.GetAsync(patientId);
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
-            _mockPatientUnitOfWork?.Verify(x => x.GetAsync(patientId), Times.Once());
+            _mockMedicUnitOfWork?.Verify(x => x.GetAsync(patientId), Times.Once());
         }
 
         [TestMethod]
         public async Task GetAsync_ReturnsOK_WhenDocumentIdMatch()
         {
-            var patient = new Patient { Name = "some name", DocumentId = "123456" };
-            var response = new ActionResponse<Patient> { WasSuccess = true, Result = patient };
-            _mockPatientUnitOfWork.Setup(x => x.GetAsync(patient.DocumentId)).ReturnsAsync(response);
+            var patient = new Medic { Name = "some name", DocumentId = "123456" };
+            var response = new ActionResponse<Medic> { WasSuccess = true, Result = patient };
+            _mockMedicUnitOfWork.Setup(x => x.GetAsync(patient.DocumentId)).ReturnsAsync(response);
 
-            var result = await _patientsController.GetAsync(patient.DocumentId);
+            var result = await _mediciansController.GetAsync(patient.DocumentId);
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
             var okResult = result as OkObjectResult;
             Assert.AreEqual(response.Result, okResult!.Value);
-            _mockPatientUnitOfWork?.Verify(x => x.GetAsync(patient.DocumentId), Times.Once());
+            _mockMedicUnitOfWork?.Verify(x => x.GetAsync(patient.DocumentId), Times.Once());
         }
 
         [TestMethod]
         public async Task GetAsync_ReturnsNotFound_WhenDocumentIdNotMatch()
         {
-            var patient = new Patient { Name = "some name", DocumentId = "123456" };
-            var response = new ActionResponse<Patient> { WasSuccess = false, Result = patient };
-            _mockPatientUnitOfWork.Setup(x => x.GetAsync(patient.DocumentId)).ReturnsAsync(response);
+            var patient = new Medic { Name = "some name", DocumentId = "123456" };
+            var response = new ActionResponse<Medic> { WasSuccess = false, Result = patient };
+            _mockMedicUnitOfWork.Setup(x => x.GetAsync(patient.DocumentId)).ReturnsAsync(response);
 
-            var result = await _patientsController.GetAsync(patient.DocumentId);
+            var result = await _mediciansController.GetAsync(patient.DocumentId);
 
             // Assert
             Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
-            _mockPatientUnitOfWork?.Verify(x => x.GetAsync(patient.DocumentId), Times.Once());
+            _mockMedicUnitOfWork?.Verify(x => x.GetAsync(patient.DocumentId), Times.Once());
         }
     }
 }
